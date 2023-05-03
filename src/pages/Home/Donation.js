2f6b1ad8f569b8babb664215a7abbffdce686c86
Siddhi-S <shethsiddhi2002@gmail.com>
2023-05-03 20:46:43 +0530

    initial commit

--- src/pages/Home/Donation.js
+++ src/pages/Home/Donation.js
@@ -9,7 +9,7 @@ const Donation = () => {
                 <img src='./assets/Logo/Found-img.jpg' alt='found-img' />
             </div>
             <div className='donation-content'>
-            <img src="./assets/Logo/logo-black-donate.png" alt='footer-logo' className='donation-content-img'/><span>Tripeasy Foundation</span>
+            <img src="./assets/Logo/logo-icon-black.png" alt='footer-logo' className='donation-content-img'/><span>Tripeasy Foundation</span>
               <h1>Help us help the planet</h1>
               <p>Together, we’ve raised more than $10 million USD for World Central Kitchen’s emergency food relief efforts around the world. This month only, donate to their Climate Disaster Fund and the Tripeasy Foundation will match it.</p>
               <button className='sign-in-btn' style={{fontWeight:'bold'}}>Donate now</button>
